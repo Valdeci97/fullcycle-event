6e76d7fe16e6f75e9fda196b6dbc78533ec69560
valdeci <moura97.valdeci@gmail.com>
2022-08-18 17:55:28 -0300

    feat: create transaction file

---
+++ codebank/domain/transaction.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type TransactionRepository interface {
+	Save(transaction Transaction, card CreditCard) error
+	GetCreditCard(card CreditCard) (CreditCard, error)
+	CreateCreditCard(card CreditCard) error
+}
+
+type Transaction struct {
+	ID          string
+	Amount      float64
+	Status      string
+	Description string
+	Store       string
+	CreditCard  string
+	CreatedAt   time.Time
+}
+
+func NewTransaction() *Transaction {
+	transaction := &Transaction{}
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+	return transaction
+}
+
+func (t *Transaction) ProccessAndValidate(card *CreditCard) {
+	if t.Amount+card.Balance > card.Limit {
+		t.Status = "Rejected"
+	} else {
+		t.Status = "Approved"
+		card.Balance = card.Balance + t.Amount
+	}
+}
